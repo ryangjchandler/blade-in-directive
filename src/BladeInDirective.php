56de6a8c95b9aef6531ed64529fd3d3a063fd7eb
Ryan Chandler <ryangjchandler@gmail.com>
2022-03-21 11:13:21 +0000

    chore: remove stuff

--- src/BladeInDirective.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace RyanChandler\BladeInDirective;
-
-class BladeInDirective
-{
-}
--- src/BladeInDirectiveServiceProvider.php
+++ src/BladeInDirectiveServiceProvider.php
@@ -4,22 +4,11 @@ namespace RyanChandler\BladeInDirective;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use RyanChandler\BladeInDirective\Commands\BladeInDirectiveCommand;
 
 class BladeInDirectiveServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('blade-in-directive')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_blade-in-directive_table')
-            ->hasCommand(BladeInDirectiveCommand::class);
+        $package->name('blade-in-directive');
     }
 }
